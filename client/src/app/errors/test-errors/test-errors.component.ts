9124e5c993e0d21cb8f9e212e225ada4e63498b9
Abdessalem Benzarti <abdessalem.benzarti@sd-c.de>
2024-08-03 13:00:16 +0200

    refactor: replace hard coded localhost

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-test-errors',
   templateUrl: './test-errors.component.html',
   styleUrls: ['./test-errors.component.css'],
 })
 export class TestErrorsComponent implements OnInit {
-  baseUrl = 'http://localhost:5000/api/';
+  baseUrl = environment.apiUrl;
   valaidationErrors: string[] = [];
 
   constructor(private http: HttpClient) {}
